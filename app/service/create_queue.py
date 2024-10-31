4b0cde429155c7ef8d6b078a0318fd8f00d45645
후니 <hipark@huniui-MacBookPro.local>
2024-11-01 01:49:52 +0900

    update env

--- app/service/create_queue.py
+++ app/service/create_queue.py
@@ -1,9 +1,18 @@
 import pika
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+RABBITMQ_CREDENTIAL1 = os.getenv('RABBITMQ_CREDENTIAL1')
+RABBITMQ_CREDENTIAL2 = os.getenv('RABBITMQ_CREDENTIAL2')
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
+RABBITMQ_PORT = os.getenv('RABBITMQ_PORT')
+
 # RabbitMQ 연결 설정
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host='localhost',
-    credentials=pika.PlainCredentials('mo', 'mo')
+    host=RABBITMQ_HOST,
+    credentials=pika.PlainCredentials(RABBITMQ_CREDENTIAL1, RABBITMQ_CREDENTIAL2)
 ))
 channel = connection.channel()
 
